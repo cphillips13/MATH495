07fa54f181151ce96853fbf46559a8fae71b100a
Corey Phillips <cep.personal@protonmail.com>
2023-03-07 09:21:12 -0500

    trying new things

--- pandastesting.py
+++ pandastesting.py
@@ -1,6 +1,32 @@
 import pandas as pd
-import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import ttest_ind
+
 
 gender_data = pd.read_csv('C:/Users/cphil/Documents/GitHub/MATH495/genderRegressionData.csv')
-gender_data.plot.scatter(x = 'S1', y = 'A2')
+
+"""
+gender_data.info()
+print(gender_data['gender'].corr(gender_data['Calories']))
+print(gender_data['gender'].corr(gender_data['Fat (g)']))
+print(gender_data['gender'].corr(gender_data['Saturated Fat (g)']))
+print(gender_data['gender'].corr(gender_data['Sodium (mg)']))
+print(gender_data['gender'].corr(gender_data['Total Carbohydrates (g)']))
+print(gender_data['gender'].corr(gender_data['Sugars (g)']))
+print(gender_data['gender'].corr(gender_data['Protein (g)']))
+"""
+
+female = gender_data[gender_data['gender'] == 1]
+male = gender_data[gender_data['gender'] == 2]
+maleAverage = {}
+
+print(maleAverage)
+print(female['Calories'].var())
+print(male['Calories'].var())
+
+print(ttest_ind(female['Calories'], male['Calories'], equal_var = False))
+
+sns.scatterplot(x = 'USERID', y = 'Calories', data = female)
+sns.scatterplot(x = 'USERID', y = 'Calories', data = male)
+plt.show()
